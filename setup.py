a39e9fa3276f3c77fec090e7e4169e781068e833
nike4613 <aaron.cirr.com@gmail.com>
2015-04-19 00:08:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+import sys
+if sys.version_info<(2,6,0):
+  sys.stderr.write("You need python 2.6 or later to run this script\n")
+  exit(1)
+
 setup(
   name='AccountsClientPython',
-  version='0.3',
+  version='0.31',
   description="A python version of Mojang's AccountsClient repository with some extra features",
-  install_requires=['requests','json'],
-  author='techkid6',
-  author_email='evan.markowitz99@gmail.com',
+  install_requires=['requests'],
+  author='DaNike4613',
+  author_email='aaron.cirr.com@gmail.com',
   packages=['AccountsClientPython']
 )
